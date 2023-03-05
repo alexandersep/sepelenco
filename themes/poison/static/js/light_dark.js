c2552d89d40e500a16c17662b7fcc8500471186f
alexandersep <sepelena@tcd.ie>
2023-03-05 17:52:58 +0000

    Removed poison folder to make repo more self contained

--- themes/poison/static/js/light_dark.js
+++
@@ -1,7 +0,0 @@
-const themeFromLS = localStorage.getItem("theme")
-const themeFromHugo = document.body.classList.contains("dark-theme") ? "dark" : null
-const currentTheme = themeFromLS ? themeFromLS : themeFromHugo;
-
-if (currentTheme == "dark") {
-    document.body.classList.add("dark-theme");
-} 
